b9637b3ed04f18f24c04c46cffcd2e04be0947b6
J Marcos <joaomarcos2004.jonh@gmail.com>
2023-05-22 22:50:56 -0300

    erros corrigidos  ++ 2 poderes adicionados

--- script.js
+++ script.js
@@ -9,18 +9,20 @@ let mouseY = false
 
 let passoX
 let passoY
-let velocidade 
+let velocidade
 let angulo
 let tamanho
+let borda
 let forma = 'neutra'
 let digestao
 let elementoComendo
+let infectando = false
 
 let colisaoTop = false
 let colisaoBottom = false
 let colisaoLeft = false
 let colisaoRight = false
-let elementoColidindo 
+let elementoColidindo
 
 
 setInterval(() => {
@@ -28,9 +30,9 @@ setInterval(() => {
     direcaoMover()
   }
   // mouseSeta() 
-  moverGlobulo()
+  moverGlobulo(globolo_branco)
   formaAtual()
-  moverTela() 
+  moverTela()
   verificarColisao()
   moverBacterias()
   moverVirus()
@@ -51,20 +53,17 @@ tela.addEventListener('mouseout', () => {
 })
 
 tela.addEventListener('mousedown', (mousedown) => {
- forma = 'comer'
+  forma = 'comer'
 })
 
 tela.addEventListener('mouseup', (mouseup) => {
- forma = 'neutra'
+  forma = 'neutra'
 })
 
 tela.addEventListener('click', (click) => {
 
 })
 
-tela.addEventListener('dblclick', (dbclick) => {
-  poder()
-})
 
 
 
@@ -98,14 +97,14 @@ function mouseSeta() {
 }
 
 
-function moverGlobulo() {
+function moverGlobulo(globolo_branco) {
   let posicaoX = globolo_branco.offsetLeft
   let posicaoY = globolo_branco.offsetTop
 
   movimentoBarrado()
 
-  posicaoX += passoX * velocidade 
-  posicaoY -= passoY * velocidade 
+  posicaoX += passoX * velocidade
+  posicaoY -= passoY * velocidade
 
   globolo_branco.style.left = posicaoX + 'px'
   globolo_branco.style.top = posicaoY + 'px'
@@ -115,7 +114,7 @@ function moverGlobulo() {
 
 
 function moverTela() {
-  window.scroll(globolo_branco.offsetLeft - window.innerWidth / 2 + globolo_branco.offsetWidth/2, globolo_branco.offsetTop - window.innerHeight / 2 + globolo_branco.offsetHeight/2)
+  window.scroll(globolo_branco.offsetLeft - window.innerWidth / 2 + globolo_branco.offsetWidth / 2, globolo_branco.offsetTop - window.innerHeight / 2 + globolo_branco.offsetHeight / 2)
 }
 
 function verificarColisao() {
@@ -128,61 +127,59 @@ function verificarColisao() {
 
   if (globolo_branco.offsetLeft > mapa.offsetLeft + mapa.offsetWidth - globolo_branco.offsetWidth) {
     colisaoRight = true
-  } else { colisaoRight = false}
+  } else { colisaoRight = false }
 
   if (globolo_branco.offsetTop < mapa.offsetTop) {
     colisaoTop = true
   } else { colisaoTop = false }
-  
+
   if (globolo_branco.offsetTop > mapa.offsetTop + mapa.offsetHeight - globolo_branco.offsetHeight) {
     colisaoBottom = true
-  } else { colisaoBottom = false}
+  } else { colisaoBottom = false }
 
-  if (colisaoBottom || colisaoLeft || colisaoRight || colisaoTop) {elementoColidindo = mapa}
+  if (colisaoBottom || colisaoLeft || colisaoRight || colisaoTop) { elementoColidindo = mapa }
 
   // verificando os corpos
   const bacterias = document.querySelectorAll('.bacteria')
   const virus = document.querySelectorAll('.virus')
-  
+
 
   bacterias.forEach((bacteria) => {
     if (checkCollision(globolo_branco, bacteria)) {
       if (bacteria != elementoComendo) {
-      elementoColidindo = bacteria}
+        elementoColidindo = bacteria
+      }
     }
   })
-  
+
   virus.forEach((virus) => {
     if (checkCollision(globolo_branco, virus)) {
       if (virus != elementoComendo) {
-      elementoColidindo = virus}
+        elementoColidindo = virus
+      }
     }
-  }) 
+  })
 
-  if (elementoColidindo) {
+  if (elementoColidindo && !elementoColidindo.classList.contains('hospedeiro')) {
     globolo_branco.style.borderColor = 'red'
   } else {
     globolo_branco.style.borderColor = 'white'
   }
 
-  if (forma != 'comer') {
-    if (elementoColidindo) {
-      globolo_branco.style.borderTop = 'transparent'
-    } else {
-      globolo_branco.style.borderTop = 'transparent'
-    }
+  if (forma == 'comer' && !digestao) {
+      globolo_branco.style.borderTop = `${borda}px solid   transparent`
   }
 
 
-  if (forma == "comer" && elementoColidindo) {
+  if (forma == "comer" && !infectando &&  elementoColidindo && elementoColidindo != mapa && !digestao && !globolo_branco.innerHTML.includes('virus_coroa') && !elementoColidindo.classList.contains('hospedeiro')) {
     comer()
-  } 
+  }
 }
 
 function checkCollision(element1, element2) {
   var rect1 = element1.getBoundingClientRect();
   var rect2 = element2.getBoundingClientRect();
-  
+
   return !(
     rect1.right < rect2.left ||
     rect1.left > rect2.right ||
@@ -205,7 +202,7 @@ function movimentoBarrado() {
   if (colisaoRight) {
     passoX > 0 ? passoX = 0 : 0
   }
-  
+
 }
 
 
@@ -213,27 +210,42 @@ function moverBacterias() { }
 function moverVirus() { }
 
 function formaAtual() {
+  const visual = globolo_branco.style
 
-if (digestao) {
-     tamanho = 100 + elementoComendo.offsetHeight/4
-     velocidade = 1
-     globolo_branco.style.opacity = '1'
+  if (digestao) {
+    tamanho = 100 + elementoComendo.offsetHeight / 4
+    velocidade = 1
+    visual.opacity = '1'
+    visual.border = '10px solid white'
+    visual.borderTop = '10px solid white'
+    visual.backgroundColor = 'white'
   } else {
 
- if (forma == "neutra"){
-  tamanho = 100
-  velocidade = 1.5
-  globolo_branco.style.opacity = '1'
-  
-}  else if (forma == "comer") {
-  tamanho = 150
-  velocidade = 1
-  globolo_branco.style.opacity = '0.5'
-}
+    if (forma == "neutra") {
+      tamanho = 100
+      velocidade = 1.7
+      borda = 10
+      visual.opacity = '1'
+      visual.border = '10px solid white'
+      visual.borderTop = '10px solid white'
+      visual.backgroundColor = 'white'
+
+      globolo_branco.childElementCount > 0 ? poderes(globolo_branco.children, 'cancelar') : 0
+
+    } else if (forma == "comer") {
+      tamanho = 0
+      velocidade = 1
+      borda = 50
+      visual.border = `${borda}px solid white`
+      visual.borderTop = `${borda}px solid   transparent`
+      visual.backgroundColor = 'transparent'
+
+      globolo_branco.childElementCount > 0 ? poderes(globolo_branco.children) : 0
+    }
   }
 
- globolo_branco.style.width = tamanho + 'px'
- globolo_branco.style.height = tamanho + 'px'
+  visual.width = tamanho + 'px'
+  visual.height = tamanho + 'px'
 
 }
 
@@ -246,26 +258,102 @@ function comer() {
   globolo_branco.appendChild(elementoColidindo)
   elementoComendo.style.opacity = '0.5'
 
-  console.log(forma,'comeu')
   digestao = true
   setTimeout(() => {
-    console.log(forma,'comeu')
     digestao = false
-  globolo_branco.innerHTML = ''
-    evoluir()
+    globolo_branco.innerHTML = ''
+    globolo_branco.innerHTML = elementoComendo.innerHTML
     elementoComendo = false
-}, 3000)
+    forma = 'neutra'
+  }, 3000)
 }
 
 
+function poderes(poderInnerHTML, cancelar) {
+  poderInnerHTML[0].classList.contains('virus_coroa') ? virusCoroa(cancelar) : 0
 
-function evoluir() {
-  globolo_branco.innerHTML = elementoComendo.innerHTML 
+  poderInnerHTML[0].classList.contains('rna') ? rna(poderInnerHTML, cancelar) : 0
+
+  poderInnerHTML[0].classList.contains('rabinho') ? rabinho(cancelar) : 0
 }
 
-function poder() {}
+
+function virusCoroa(cancelar) {
+  const coroa = document.querySelector('.virus_coroa')
+  coroa.style.borderTop = '10px dashed transparent'
+  tamanho = 50
+  borda = 25
+  globolo_branco.style.border = `${borda}px dashed white`
+
+ if (cancelar) {
+    coroa.style.borderTop = '10px dashed white'
+  }
+
+  if (forma == "comer" && !infectando &&  elementoColidindo && elementoColidindo != mapa && !digestao && !elementoColidindo.classList.contains('hospedeiro') && !elementoColidindo.classList.contains('virus')) {infectar()}
+  
+
+  if (infectando && !cancelar && forma == 'comer') {
+    const cadaver = document.createElement('div')
+    cadaver.classList.add('corpo')
+    cadaver.style.opacity = '0.3'
+    cadaver.innerHTML = globolo_branco.innerHTML
+    globolo_branco.innerHTML = ''
+    cadaver.style.position = 'absolute'
+    cadaver.style.top = globolo_branco.style.top
+    cadaver.style.left = globolo_branco.style.left
+    cadaver.style.width = 'fit-content'
+    cadaver.style.height = 'fit-content'
+    mapa.appendChild(cadaver)
+
+    elementoComendo = false
+    infectando = false
+  }
+
+}
+
+function infectar(cancelar) {
+
+ infectando = elementoColidindo
+ infectando.classList.add('hospedeiro')
+ globolo_branco.appendChild(infectando)
+ const hospedeiro = globolo_branco.querySelector('.hospedeiro')
+ hospedeiro.style.width = elementoColidindo.style.width
+ hospedeiro.style.height = elementoColidindo.style.height
+ 
+ 
+forma = 'neutra'
+ 
+}
+
+function rna(barras, cancelar) {
+  if (barras.length > 1 && !cancelar) {
+    for (c = 1; c < barras.length; c++) {
+      const clone = document.createElement('div')
+      clone.id = 'globolo_branco'
+      clone.classList.add('clone')
+      clone.innerHTML = '<div class="rna"></div>'
+      clone.style.top = globolo_branco.offsetTop + 'px'
+      clone.style.left = globolo_branco.offsetLeft + 'px'
+
+      mapa.appendChild(clone)
+      globolo_branco.innerHTML = '<div class="rna"></div>'
+
+    }
+  } else if (!cancelar) {
+    const clones = document.querySelectorAll('.clone')
+    clones.forEach((clone) => {
+      moverGlobulo(clone)
+
+    })
+  }
 
 
+
+}
+
+function rabinho(rabinho) {
+  console.log('vum!')
+}
 
 
 
